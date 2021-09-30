9245673d008197656d46b875c4c6c2b0e51ea766
naman0058 <jnaman345@gmail.com>
2021-09-30 10:44:40 +0530

    a

--- routes/pool.js
+++ routes/pool.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
  
  host : 'db-mysql-blr1-56981-do-user-9676911-0.b.db.ondigitalocean.com',
    user: 'doadmin',
-    password : '  ',
+    password : 'a0xe66knpqoeoy0i',
     database: 'lapify',
     port:'25060' ,
     multipleStatements: true
