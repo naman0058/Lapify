d252872779a27d242d315d1068f79b0fceb4dec2
naman0058 <jnaman345@gmail.com>
2021-10-12 09:38:32 +0530

    a

--- routes/index.js
+++ routes/index.js
@@ -7,9 +7,88 @@ const fs = require("fs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var query = `select * from category where type = 'sell_laptop' order by name ;`
+  pool.query(query,(err,result)=>{
+    if(err) throw err;
+    else res.render('index', { title: 'Express',result:result });
+  })
+  // res.render('index', { title: 'Express' });
 });
 
+
+
+
+
+router.get('/sell-old-laptop', function(req, res, next) {
+  var query = `select * from category where type = 'sell_laptop' order by name ;`
+  pool.query(query,(err,result)=>{
+    if(err) throw err;
+    else res.render('show_brand', { title: 'Express',result:result });
+  })
+  // res.render('index', { title: 'Express' });
+});
+
+
+
+router.get('/sell-old-desktop', function(req, res, next) {
+  var query = `select * from category where type = 'sell_desktop' order by name ;`
+  pool.query(query,(err,result)=>{
+    if(err) throw err;
+    else res.render('show_brand', { title: 'Express',result:result });
+  })
+  // res.render('index', { title: 'Express' });
+});
+
+
+
+router.get('/sell-old-accessories', function(req, res, next) {
+  var query = `select * from category where type = 'sell_accessories' order by name ;`
+  pool.query(query,(err,result)=>{
+    if(err) throw err;
+    else res.render('show_brand', { title: 'Express',result:result });
+  })
+  // res.render('index', { title: 'Express' });
+});
+
+
+router.get('/enquiry', function(req, res, next) {
+  var query = `select * from category where type = 'sell_laptop' order by name ;`
+  pool.query(query,(err,result)=>{
+    if(err) throw err;
+    else res.render('show_brand', { title: 'Express',result:result });
+  })
+  // res.render('index', { title: 'Express' });
+});
+
+
+
+
+router.get('/brand/:name/:id',(req,res)=>{
+  pool.query(`select s.* , 
+  (select b.name from brand b where b.id = s.brandid) as brandname
+   from model s where s.brandid = '${req.params.id}'  order by name  `,(err,result)=>{
+  if(err) throw err;
+      else res.render('show_model',{result:result})
+})
+})
+
+
+
+router.get('/model/:name/:id',(req,res)=>{
+	pool.query(`select s.* , 
+    (select b.name from brand b where b.id = s.brandid) as brandname,
+    (select b.value from specification b where b.id = s.ram) as ramvalue,
+    (select b.price from specification b where b.id = s.ram) as ramprice,
+    (select b.value from specification b where b.id = s.processor) as processorvalue,
+    (select b.price from specification b where b.id = s.processor) as processorprice,
+    (select b.value from specification b where b.id = s.harddisk) as harddiskvalue,
+    (select b.price from specification b where b.id = s.harddisk) as harddiskprice
+     from model s where s.id = '${req.params.id}' `,(err,result)=>{
+		if(err) throw err;
+    // else res.json(result)
+         else res.render('single_model',{result:result})
+	})
+})
 
 
 
@@ -206,6 +285,111 @@ router.get('/bulkenquiry/all',(req,res)=>{
 
    })
 })
+
+
+
+
+router.get('/switchon',(req,res)=>{
+  res.render('switchon')
+})
+
+
+
+
+
+
+// CustomNumber
+// amount
+
+
+
+
+// router.post('/get_recharge',(req,res)=>{
+//   let body = req.body;
+//   pool.query(`select * from users where mobile = '${req.body.mobile}' and (type = 'Retailer' || type = 'Distributor') and status = 'Active' and main_balance > '${req.body.amount}'`,(err,result)=>{
+//       if(err) throw err;
+//       else if(result[0]){
+//        pool.query(`select * from transactions where customer_no = '${req.body.number}' and amount = '${req.body.amount}' and status = 'success' and time query`,(err,result)=>{
+//            if(err) throw err;
+//            else if(result[0]){
+//             res.json({msg:'Duplicate Transaction'})
+//            }
+//            else{
+//         pool.query(`insert into transaction set ?`,body,(err,result)=>{
+//             if(err) throw err;
+//             else {
+//                 pool.query(`update users set main_balance = main_balance - '${req.body.amount}' where mobile = '${req.body.mobile}'`,(err,result)=>{
+//                     if(err) throw err;
+//                     else {
+//                         // calling api here...
+//                     }
+//                 })
+//             }
+//         })
+
+//            }
+//        })
+//       }
+//       else{
+//           res.json({msg:'Invalid Number or Balance Low'})
+//       }
+//   })
+// })
+
+
+
+// router.post('/ussd_recharge',(req,res)=>{
+//   let body = req.body;
+//   console.log(req.body)
+//   // res.json({response : body})
+
+// //code checking for balance 
+// if(req.body.reqText == '*370*1#'){
+
+
+//   pool.query(`select main_balance from users where mobile = '${req.body.msisdn}' and status = 'active'`,(err,result)=>{
+//     if(err) throw err;
+//     else if(result[0]){
+//       res.json(result)
+//     }
+//     else{
+//      res.json({msg : 'You are not active or not registered'})
+//     }
+//   })
+
+// }
+
+
+// // reqtext
+// else if(req.body.reqText == '*370*{9}*{4}#'){
+
+//   receivernumber=9;
+//   70 or 71 -> Awc_recharge()
+//   79 or 73 -> Roshan_recharge()
+//   78 -> etisatlat_recharge()
+//   74 -> salaam_recharge()
+//   72 -> mtn recharge()
+//   amount=4;
+
+// }
+// //Transfering amount from one top client ot the down client
+// else if(req.body.reqText == '*370*code*amount#'){
+
+//   //check if active or not
+//   //check if user has enough amount - > main_balance -> user_type condition
+//   // duplicate condition -> same time & same amount ->(fund_transfer table)
+//   // sender deduct and receiver add 
+//   // fund_transfer table insert data
+//   //success or failed return
+//   //sms send -> your amount 400 inr has transfered successfuly to code number.
+//   //response to the screen 
+
+// }
+
+
+
+// })
+
 
 
 module.exports = router;
--- routes/pool.js
+++ routes/pool.js
@@ -3,15 +3,33 @@ var mysql = require('mysql')
 require('dotenv').config()
 
 const pool = mysql.createPool({
- 
- host : 'db-mysql-blr1-56981-do-user-9676911-0.b.db.ondigitalocean.com',
-   user: 'doadmin',
-    password : 'a0xe66knpqoeoy0i',
-    database: 'lapify',
-    port:'25060' ,
-    multipleStatements: true
+  host : 'db-mysql-blr1-56981-do-user-9676911-0.b.db.ondigitalocean.com',
+  user: 'doadmin',
+  password : 'a0xe66knpqoeoy0i',
+  database: 'lapify',
+  port:'25060' ,
+  multipleStatements: true
   })
 
 
 module.exports = pool;
+
+
+
+
+// var mysql = require('mysql')
+// require('dotenv').config()
+
+// const pool = mysql.createPool({
+ 
+//  host : 'localhost',
+//    user: 'root',
+//     password : '123',
+//     database: 'recharge',
+//     port:'3306' ,
+//     multipleStatements: true
+//   })
+
+
+// module.exports = pool;
 
