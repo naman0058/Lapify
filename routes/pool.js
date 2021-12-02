e40998863e90e016d50d32874b849350dc2dbd29
naman0058 <jnaman345@gmail.com>
2021-12-02 13:03:28 +0530

    a

--- routes/pool.js
+++ routes/pool.js
@@ -3,33 +3,14 @@ var mysql = require('mysql')
 require('dotenv').config()
 
 const pool = mysql.createPool({
-  host : 'db-mysql-blr1-56981-do-user-9676911-0.b.db.ondigitalocean.com',
-  user: 'doadmin',
-  password : 'a0xe66knpqoeoy0i',
-  database: 'lapify',
-  port:'25060' ,
-  multipleStatements: true
+ 
+ host : 'db-mysql-blr1-79982-do-user-10297867-0.b.db.ondigitalocean.com',
+   user: 'doadmin',
+    password : 'nc3PdpfQIcW8tOFm',
+    database: 'lapify',
+    port:'25060',
+    multipleStatements: true
   })
 
 
 module.exports = pool;
-
-
-
-
-// var mysql = require('mysql')
-// require('dotenv').config()
-
-// const pool = mysql.createPool({
- 
-//  host : 'localhost',
-//    user: 'root',
-//     password : '123',
-//     database: 'recharge',
-//     port:'3306' ,
-//     multipleStatements: true
-//   })
-
-
-// module.exports = pool;
-
